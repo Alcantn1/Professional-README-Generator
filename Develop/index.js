eb04fdd1bbfada6878e7122bbd59e06709108ccf
Alcantn1 <nalcantara92@gmail.com>
2023-05-17 21:30:11 -0700

    added arrays

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,62 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer'),
+const fs = require('fs');
+const path =require('path');
+const generateMarkDown = require('./utils/generateMarkDown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type: 'input'  ,
+    name: 'github'  ,
+    message:'what is your github username?',
+},
+{
+    type: 'input'  ,
+    name: 'email'  ,
+    message:'what is your email',
+},
+{
+    type: 'input'  ,
+    name:  'title' ,
+    message: ' what is the title of your project?',
+},
+{
+    type:  'input' ,
+    name:  'description' ,
+    message:'give a description of your project',
+},
+{
+    type: 'input'  ,
+    name: 'installation' ,
+    message:'what do you need to do to install this project?',
+},
+{
+    type: 'input'  ,
+    name: 'usage'  ,
+    message:'what is your project used for?',
+},
+{
+    type: 'input'  ,
+    name: 'license'  ,
+    message:'wjat type of license does your project have?',
+},
+{
+    type: 'input' ,
+    name: 'dependencies' ,
+    message: '' ,
+},
+{
+    type: 'input' ,
+    name: 'tests' ,
+    message: 'how do you run tests on this project?' ,
+},
+{
+    type: 'input' ,
+    name: 'contributors' ,
+    message: ''who contributed to your project?' ,
+},
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
