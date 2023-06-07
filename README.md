50de021060a1300b5c1122f1901a3b5b32aed619
Alcantn1 <nalcantara92@gmail.com>
2023-06-06 18:49:38 -0700

    put generate markdown code into the index.js file together

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Professional-README-Generator
+# Professional readME generator
+
+  ## Description
+this is a read me generator that allows you to answer the prompted questions so it can successfully create a readME file.
+## Installation
+type npm i into the command line.
+## Usage
+this project is used to create a professioanl readME for future projects.
+## Contributing
+Nico A.
+## Tests
+
+  ## License
+  ![GitHub license](https://img.shields.io/badge/license-mit licence-blue.svg)
+  License: [mit licence](LICENSE.md)
+
+
--- index.js
+++ index.js
@@ -1,84 +1,124 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const path =require('path');
-const generateMarkdown = require('/Users/nicoalcantara/node-challenge/Professional-README-Generator/index.js');
+const path = require('path');
 
-// TODO: Create an array of questions for user input
+// array of questions for user input
 const questions = [
-{
-    type: 'input'  ,
-    name: 'github'  ,
-    message:'what is your github username?',
-},
-{
-    type: 'input'  ,
-    name: 'email'  ,
-    message:'what is your email',
-},
-{
-    type: 'input'  ,
-    name:  'title' ,
-    message: ' what is the title of your project?',
-},
-{
-    type:  'input' ,
-    name:  'description' ,
-    message:'give a description of your project',
-},
-{
-    type: 'input'  ,
-    name: 'installation' ,
-    message:'what do you need to do to install this project?',
-},
-{
-    type: 'input'  ,
-    name: 'usage'  ,
-    message:'what is your project used for?',
-},
-{
-    type: 'input'  ,
-    name: 'license'  ,
-    message:'what type of license does your project have?',
-},
-{
-    type: 'input' ,
-    name: 'tests' ,
-    message: 'how do you run tests on this project?' ,
-},
-{
-    type: 'input' ,
-    name: 'contributors' ,
-    message: 'who contributed to your project?' ,
-},
-];
+  {
+        type: 'input'  ,
+        name: 'github'  ,
+        message:'what is your github username?',
+    },
+    {
+        type: 'input'  ,
+        name: 'email'  ,
+        message:'what is your email',
+    },
+    {
+        type: 'input'  ,
+        name:  'title' ,
+        message: ' what is the title of your project?',
+    },
+    {
+        type:  'input' ,
+        name:  'description' ,
+        message:'give a description of your project',
+    },
+    {
+        type: 'input'  ,
+        name: 'installation' ,
+        message:'what do you need to do to install this project?',
+    },
+    {
+        type: 'input'  ,
+        name: 'usage'  ,
+        message:'what is your project used for?',
+    },
+    {
+        type: 'input'  ,
+        name: 'license'  ,
+        message:'what type of license does your project have?',
+    },
+    {
+        type: 'input' ,
+        name: 'tests' ,
+        message: 'how do you run tests on this project?' ,
+    },
+    {
+        type: 'input' ,
+        name: 'contributors' ,
+        message: 'who contributed to your project?' ,
+    },
+    ];
+;
 
-// TODO: Create a function to write README file
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return '';
+}
+
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `License: [${license}](LICENSE.md)`;
+  }
+  return '';
+}
+
+function renderLicenseSection(license) {
+  const licenseBadge = renderLicenseBadge(license);
+  const licenseLink = renderLicenseLink(license);
+  if (licenseBadge || licenseLink) {
+    return `
+  ## License
+  ${licenseBadge}
+  ${licenseLink}
+`;
+  }
+
+  return '';
+}
+
+function generateMarkdown(data) {
+  const licenseSection = renderLicenseSection(data.license);
+  return `# ${data.title}
+
+  ## Description
+${data.description}
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## Contributing
+${data.contributors}
+## Tests
+${licenseSection}
+
+`;
+}
+
+// function to write README file
 function writeToFile(fileName, data) {
-    const outputPath = path.join(process.cwd(), fileName);
-    fs.writeFile(outputPath, data, (err) => {
-      if (err) {
-        console.error('An error occurred while writing the file:', err);
-      } else {
-        console.log('README file created successfully!');
-      }
-    });
-  }
+  const outputPath = path.join(process.cwd(), fileName);
+  fs.writeFile(outputPath, data, (err) => {
+    if (err) {
+      console.error('An error occurred while writing the file:', err);
+    } else {
+      console.log('README file created successfully!');
+    }
+  });
+}
 
-// TODO: Create a function to initialize app
-// function init() {
-//     inquirer.prompt(questions).then((data) => {
-//         const markdown = generateMarkdown(data);
-//         writeToFile('README.md', generateMarkdown);
-//       });
-// }
+module.exports = generateMarkdown;
+
 function init() {
-    inquirer.prompt(questions).then((data) => {
-      const markdown = generateMarkdown(data);
-      writeToFile('README.md', generateMarkdown); 
-    });
-  }
-  
+  inquirer.prompt(questions).then((data) => {
+    const markdown = generateMarkdown(data);
+    writeToFile('README.md', markdown);
+  });
+}
 
 // Function call to initialize app
 init();
+
